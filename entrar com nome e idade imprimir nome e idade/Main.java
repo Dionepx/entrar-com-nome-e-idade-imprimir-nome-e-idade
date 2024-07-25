3bbc25e9fc4692f5ffa452f1906bef8b5e500d61
Dione V. Silva <135567312+Dionepx@users.noreply.github.com>
2024-07-24 23:31:19 -0300

    Add files via upload

---
+++ entrar com nome e idade imprimir nome e idade/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+    }
+}
